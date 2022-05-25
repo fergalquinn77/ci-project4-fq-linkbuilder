1c3a681728550d4ae74b228c1bde962a88c870aa
fergalquinn77 <fergal@bourbon.ie>
2022-05-25 19:25:45 +0000

    Remove disable static from Heroku

--- links/tests.py
+++ links/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class URLTests(TestCase):
+    def test_testhomepage(self):
+        response=self.client.get('/')
+        self.assertAlmostEqual(response.status.code, 200)
