0133468f2ad12a4557c67ea5a86da0b88009849b
fergalquinn77 <fergal@bourbon.ie>
2022-06-05 22:24:53 +0000

    Add test for add links page and re-run coverage report

--- links/test_views.py
+++ links/test_views.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from .models import Url_Links
 from django.contrib.auth import get_user_model
+from django.test import Client
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 class TestViews(TestCase):
 
@@ -10,8 +13,24 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'links/learn.html')
 
+# Test External View Page
     def test_external_page(self):
         test_user = get_user_model().objects.create(username="foo", email="test@test.com", password="bar")
-        response = self.client.get((f'/user/{test_user.username}'))
+        response = self.client.get(f'/user/{test_user.username}/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'links/index_external.html')
+
+#Test Add Link Page
+    def test_add_link_page(self):
+        user = User.objects.create_user('foo', 'myemail@test.com', 'bar')
+        self.client.login(username='foo', password='bar')
+        response = self.client.get('/add/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'links/add_item.html')
+
+# #Test Edit Link Page
+#     def test_add_link_page(self):
+#         user = User.objects.create_user('foo', 'myemail@test.com', 'bar')
+#         self.client.login(username='foo', password='bar')
+#         response = self.client.get('/edit/')
+#         self.assertEqual(response.status_code, 200)
