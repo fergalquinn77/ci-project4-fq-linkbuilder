db922da797c2d044d60f4ccb2512db9948ea76fb
fergalquinn77 <fergal@bourbon.ie>
2022-06-15 16:34:35 +0000

    Change settings file to take out use of +

--- linktree/settings.py
+++ linktree/settings.py
@@ -136,20 +136,16 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': ('django.contrib.auth.password_validation.' +
-                 'UserAttributeSimilarityValidator'),
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', # noqa: E501
     },
     {
-        'NAME': ('django.contrib.auth.password_validation.' +
-                 'MinimumLengthValidator'),
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', # noqa: E501
     },
     {
-        'NAME': ('django.contrib.auth.password_validation.' +
-                 'CommonPasswordValidator'),
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', # noqa: E501
     },
     {
-        'NAME': ('django.contrib.auth.password_validation.' +
-                 'NumericPasswordValidator'),
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', # noqa: E501
     },
 ]
 
@@ -171,8 +167,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = ('cloudinary_storage.storage.' +
-                       'StaticHashedCloudinaryStorage')
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage' # noqa: E501
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
